6f0597a46cd22c02dec6f6c5b6afd995f7c86876
YOUNGmaxer <517538470@qq.com>
2023-03-07 19:23:48 +0800

    fix: 修复请求异常

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const Koa = require('koa')
 const app = new Koa()
 const eventHander = require('./index')
 
-app.use(async (ctx, next) => {
-    ctx.body = await eventHander(ctx.body, ctx)
+app.use(async ctx => {
+    ctx.body = await eventHander(ctx.body || {}, ctx)
 })
 
 app.listen(9000, () => {
