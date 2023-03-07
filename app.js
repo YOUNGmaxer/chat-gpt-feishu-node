c4bfbe50388dab278b0d1947276a39e3e4fb2c72
YOUNGmaxer <517538470@qq.com>
2023-03-07 19:37:53 +0800

    fix: 修复body读取异常

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 const eventHander = require('./index')
 
+app.use(bodyParser())
+
 app.use(async ctx => {
-    ctx.body = await eventHander(ctx.body || {}, ctx)
+    ctx.body = await eventHander(ctx.request.body || {}, ctx)
 })
 
 app.listen(9000, () => {
